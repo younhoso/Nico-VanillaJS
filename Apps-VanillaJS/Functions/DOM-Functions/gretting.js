132afcf4e161aa0df85641135955bf6f38a68c53
younhoso <thdbsgh3443@naver.com>
2019-12-14 16:49:46 +0900

    191214 2 커밋

--- Apps-VanillaJS/Functions/DOM-Functions/gretting.js
+++ Apps-VanillaJS/Functions/DOM-Functions/gretting.js
@@ -1,43 +1,45 @@
-var form = document.querySelector('.js-form'),
-    input = form.querySelector('input'),
-    greeting = document.querySelector('.js-greetings');
+(function(){
+    var form = document.querySelector('.js-form'),
+        input = form.querySelector('input'),
+        greeting = document.querySelector('.js-greetings');
 
-var userLs = "currentUser",
-    showing_cn = "showing";
+    var userLs = "currentUser",
+        showing_cn = "showing";
 
-var saveName = function(text) {
-    localStorage.setItem(userLs, text)
-};
+    var saveName = function(text) {
+        localStorage.setItem(userLs, text)
+    };
 
-var paintGreeting = function(text) {
-    form.classList.remove(showing_cn);
-    greeting.classList.add(showing_cn);
-    greeting.innerText = `Hello ${text}`
-}
-
-var handleSubmit = function(e){
-    e.preventDefault();
-    var currentValue = input.value;
-    paintGreeting(currentValue);
-    saveName(currentValue);
-}
-
-var askForNmae = function() {
-    form.classList.add(showing_cn);
-    form.addEventListener('submit', handleSubmit);
-};
-
-var loadNmae = function() {
-    var currentUser = localStorage.getItem(userLs);
-    if(currentUser === null){
-        askForNmae();
-    } else {
-        paintGreeting(currentUser)
+    var paintGreeting = function(text) {
+        form.classList.remove(showing_cn);
+        greeting.classList.add(showing_cn);
+        greeting.innerText = `Hello ${text}`
     }
-};
 
-var init = function() {
-    loadNmae();
-};
+    var handleSubmit = function(e){
+        e.preventDefault();
+        var currentValue = input.value;
+        paintGreeting(currentValue);
+        saveName(currentValue);
+    }
 
-init();
+    var askForNmae = function() {
+        form.classList.add(showing_cn);
+        form.addEventListener('submit', handleSubmit);
+    };
+
+    var loadNmae = function() {
+        var currentUser = localStorage.getItem(userLs);
+        if(currentUser === null){
+            askForNmae();
+        } else {
+            paintGreeting(currentUser)
+        }
+    };
+
+    var init = function() {
+        loadNmae();
+    };
+
+    init();
+})();
